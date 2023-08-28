01d24ef1a452695e24c61a6cd66043c289241246
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-08-29 00:20:30 +0300

    create hero section data

---
+++ src/data.jsx
@@ -0,0 +1,33 @@
+import risk from "../src/assets/Images/risk-protection.jpg";
+import patient from "../src/assets/Images/patient.jpg";
+import family from "../src/assets/Images/happy-black-family.jpg";
+import travel from "../src/assets/Images/travelinsur.jpg";
+import carinsur from "../src/assets/Images/upset-woman.jpg";
+
+export const slide = [
+  {
+    url: carinsur,
+    title: "Protecting Your Peace of Mind",
+    description: "Comprehensive Insurance Solutions Tailored Just for You",
+  },
+  {
+    url: travel,
+    title: "Your Safety, Our Priority",
+    description: "Safeguarding Your Future with Trusted Insurance Plans",
+  },
+  {
+    url: family,
+    title: "Coverage You Can Count On",
+    description: "Reliable Insurance Services to Secure Your Dreams",
+  },
+  {
+    url: patient,
+    title: "Insurance Made Easy",
+    description: "Simplified Solutions for a Stress-Free Future",
+  },
+  {
+    url: risk,
+    title: "Chayil Insurance - Your Shield",
+    description: "Providing Assurance and Protection When You Need It",
+  },
+];
