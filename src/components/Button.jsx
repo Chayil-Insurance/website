f323ab8d6a792ca453b553fd3120c3dbceac4125
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-08-30 00:46:26 +0300

    change to primary and secondary color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ type, name }) => {
   return (
     <button
       type={type}
-      className="rounded-xl bg-blue-500 px-5 py-2 text-base mb-3 font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700"
+      className="rounded-xl text-[#00165A] px-5 py-2 text-base mb-3 font-medium bg-[#BFCFFF] transition duration-200 hover:bg-brand-600 active:bg-brand-700"
     >
       {name}
     </button>
