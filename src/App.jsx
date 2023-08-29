be21907426620470fd05f8d9155a15c741554de9
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-08-30 00:48:57 +0300

    factoriing

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
+import Contactpage from "./pages/Contactpage";
+import Aboutpage from "./pages/Aboutpage";
+import Productspage from "./pages/Productspage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-          <Route element={<LandingPage />} path="/" />
-          <Route element={<Login />} path="/login" />
-          <Route element={<Signup />} path="/sign-up" />
+        <Route element={<LandingPage />} path="/" />
+        <Route element={<Contactpage />} path="/contact-us" />
+        <Route element={<Aboutpage />} path="/about-us" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Signup />} path="/sign-up" />
+        <Route element={<Productspage />} path="/products" />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
