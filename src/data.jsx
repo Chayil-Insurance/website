1447ddc6aaa20c09a9f41c0ee2e2d49dfa15151f
Josphat Mwangi <josphatwanjiruw@gmail.com>
2024-01-02 20:46:39 +0300

    add services data set

--- src/data.jsx
+++ src/data.jsx
@@ -1,90 +1,139 @@
-import risk from "../src/assets/Images/risk-protection.jpg";
-import patient from "../src/assets/Images/patient.jpg";
-import family from "../src/assets/Images/happy-black-family.jpg";
-import travel from "../src/assets/Images/travelinsur.jpg";
-import carinsur from "../src/assets/Images/upset-woman.jpg";
+import risk from '../src/assets/Images/risk-protection.jpg'
+import patient from '../src/assets/Images/patient.jpg'
+// import family from '../src/assets/Images/happy-black-family.jpg'
+import travel from '../src/assets/Images/travelinsur.jpg'
+import carinsur from '../src/assets/Images/upset-woman.jpg'
 
 export const slide = [
   {
     url: carinsur,
-    title: "Protecting Your Peace of Mind",
-    description: "Comprehensive Insurance Solutions Tailored Just for You",
+    title: 'Protecting Your Peace of Mind',
+    description: 'Comprehensive Insurance Solutions Tailored Just for You',
   },
   {
     url: travel,
-    title: "Your Safety, Our Priority",
-    description: "Safeguarding Your Future with Trusted Insurance Plans",
+    title: 'Your Safety, Our Priority',
+    description: 'Safeguarding Your Future with Trusted Insurance Plans',
   },
-  {
-    url: family,
-    title: "Coverage You Can Count On",
-    description: "Reliable Insurance Services to Secure Your Dreams",
-  },
+  // {
+  //   url: family,
+  //   title: "Coverage You Can Count On",
+  //   description: "Reliable Insurance Services to Secure Your Dreams",
+  // },
   {
     url: patient,
-    title: "Insurance Made Easy",
-    description: "Simplified Solutions for a Stress-Free Future",
+    title: 'Insurance Made Easy',
+    description: 'Simplified Solutions for a Stress-Free Future',
   },
   {
     url: risk,
-    title: "Chayil Insurance - Your Shield",
-    description: "Providing Assurance and Protection When You Need It",
+    title: 'Chayil Insurance - Your Shield',
+    description: 'Providing Assurance and Protection When You Need It',
   },
-];
+]
 
 export const faq = [
   {
-    title: " What types of insurance does Chayil Insurance Agency offer?",
+    title: ' What types of insurance does Chayil Insurance Agency offer?',
     description:
-      "At Chayil Insurance Agency, we offer a comprehensive range of insurance coverage, including auto insurance, home insurance, life insurance, health insurance, business insurance, travel insurance, and specialty insurance to meet your unique needs.",
+      'At Chayil Insurance Agency, we offer a comprehensive range of insurance coverage, including auto insurance, home insurance, life insurance, health insurance, business insurance, travel insurance, and specialty insurance to meet your unique needs.',
   },
   {
-    title: "How can I get a quote for insurance coverage?",
+    title: 'How can I get a quote for insurance coverage?',
     description:
       "Getting a quote with Chayil Insurance Agency is easy. You can request a quote by visiting our website, calling our dedicated team, or visiting one of our local offices. We're here to help you find the coverage that fits your budget.",
   },
   {
-    title: "What factors affect my insurance premium?",
+    title: 'What factors affect my insurance premium?',
     description:
-      "Several factors can influence your insurance premium, including your age, driving record, location, coverage type, and the value of the insured property. Our agents will work closely with you to provide an accurate quote tailored to your specific situation.",
+      'Several factors can influence your insurance premium, including your age, driving record, location, coverage type, and the value of the insured property. Our agents will work closely with you to provide an accurate quote tailored to your specific situation.',
   },
   {
     title:
-      "Can I bundle my insurance policies with Chayil Insurance Agency for discounts?",
+      'Can I bundle my insurance policies with Chayil Insurance Agency for discounts?',
     description:
-      "Yes, we offer bundling options that allow you to combine multiple insurance policies, such as auto and home insurance, for potential discounts. Bundling is a great way to save on your premiums while simplifying your coverage.",
+      'Yes, we offer bundling options that allow you to combine multiple insurance policies, such as auto and home insurance, for potential discounts. Bundling is a great way to save on your premiums while simplifying your coverage.',
   },
   {
-    title: "How do I file an insurance claim with Chayil Insurance Agency?",
+    title: 'How do I file an insurance claim with Chayil Insurance Agency?',
     description:
-      "Filing a claim is a straightforward process with Chayil Insurance Agency. You can contact our claims department directly, either by phone or through our website. Our experienced team will guide you through the steps to ensure a smooth and efficient claims process.",
+      'Filing a claim is a straightforward process with Chayil Insurance Agency. You can contact our claims department directly, either by phone or through our website. Our experienced team will guide you through the steps to ensure a smooth and efficient claims process.',
   },
   {
-    title: "Do you offer coverage for businesses of all sizes?",
+    title: 'Do you offer coverage for businesses of all sizes?',
     description:
-      "Yes, we provide business insurance solutions that cater to businesses of all sizes, from startups to established enterprises. Our goal is to protect your business, your employees, and your future.",
-  },
-  {
-    title:
-      "What makes Chayil Insurance Agency different from other insurance providers?",
-    description:
-      "Chayil Insurance Agency stands out through our commitment to personalized service, competitive rates, and a diverse range of coverage options. We prioritize your unique needs, ensuring you receive the protection you deserve.",
+      'Yes, we provide business insurance solutions that cater to businesses of all sizes, from startups to established enterprises. Our goal is to protect your business, your employees, and your future.',
   },
   {
     title:
-      "Are there additional coverage options available for unique situations or assets?",
+      'What makes Chayil Insurance Agency different from other insurance providers?',
     description:
-      "Absolutely. Chayil Insurance Agency offers specialty insurance coverage tailored to unique situations and assets. Whether you have specific valuables, high-value items, or unique risks, we can design a policy to address your concerns.",
+      'Chayil Insurance Agency stands out through our commitment to personalized service, competitive rates, and a diverse range of coverage options. We prioritize your unique needs, ensuring you receive the protection you deserve.',
+  },
+  {
+    title:
+      'Are there additional coverage options available for unique situations or assets?',
+    description:
+      'Absolutely. Chayil Insurance Agency offers specialty insurance coverage tailored to unique situations and assets. Whether you have specific valuables, high-value items, or unique risks, we can design a policy to address your concerns.',
   },
   {
     title: "Can I make changes to my insurance policy after it's been issued?",
     description:
-      "Yes, we understand that life circumstances change. You can make adjustments to your policy at any time, whether you need to update coverage limits, add or remove coverage, or make other modifications. Our agents are here to assist you.",
+      'Yes, we understand that life circumstances change. You can make adjustments to your policy at any time, whether you need to update coverage limits, add or remove coverage, or make other modifications. Our agents are here to assist you.',
   },
   {
     title:
-      "How can I reach Chayil Insurance Agency for assistance or inquiries?",
+      'How can I reach Chayil Insurance Agency for assistance or inquiries?',
     description:
-      "You can reach us through our website, by phone, or by visiting one of our local offices. Our dedicated team is ready to assist you with any questions or concerns you may have about your insurance coverage.",
+      'You can reach us through our website, by phone, or by visiting one of our local offices. Our dedicated team is ready to assist you with any questions or concerns you may have about your insurance coverage.',
   },
-];
+]
+
+export const services = [
+  {
+    title: 'Life Insurance',
+    description:
+      'Provides financial protection for your loved ones in the event of your death',
+  },
+  {
+    title: 'Health Insurance',
+    description:
+      'Covers medical expenses, ensuring you receive necessary healthcare without a heavy financial burden.',
+  },
+
+  {
+    title: 'My Family',
+    description:
+      "Comprehensive coverage for your family's well-being, combining aspects of life, health, and other essential insurances.",
+  },
+  {
+    title: 'Investment Insurance',
+    description:
+      'Offers a combination of insurance and investment, helping you build wealth while ensuring financial security.',
+  },
+  {
+    title: 'House Insurance',
+    description:
+      'Safeguards your home and possessions against damage or loss due to various unforeseen events.',
+  },
+  {
+    title: 'Car Insurance',
+    description:
+      'Protects your vehicle against accidents, theft, and other damages, providing financial coverage and peace of mind.',
+  },
+  {
+    title: 'Travel Insurance',
+    description:
+      'Ensures coverage for unexpected events during your travels, including medical emergencies, trip cancellations, and more.',
+  },
+  {
+    title: 'Education Insurance',
+    description:
+      "Secures your child's educational future by providing financial support for their academic endeavors.",
+  },
+  {
+    title: 'Retirement Insurance',
+    description:
+      'Aims to ensure a comfortable and financially secure retirement by offering a mix of savings and insurance benefits.',
+  },
+]
